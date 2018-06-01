359b213a539cb7c6885e9f4bebfd2b4051a21298
Saulius Beinorius <saulius.beinorius@gmail.com>
2018-06-01 22:30:06 +0300

    add region data and token data class

---
+++ src/js/TokenMeta.js
@@ -0,0 +1,9 @@
+export default class TokenMeta {
+    constructor(tokenId, serialNumber, denomination, cityName, image) {
+        this.tokenId = tokenId;
+        this.serialNumber = serialNumber;
+        this.denomination = denomination;
+        this.cityName = cityName;
+        this.image = image;
+    }
+}
